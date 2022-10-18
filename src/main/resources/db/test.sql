8b681ce94a1c265a04353efdfc86b6ee1e24043c
Marcus <60523971+Simpada@users.noreply.github.com>
2022-10-18 14:07:02 +0200

    🧧 Does not compile, many sæd

---
+++ src/main/resources/db/test.sql
@@ -0,0 +1,14 @@
+create user test password 'super_safe_password';
+
+create database testDB with owner test;
+
+
+create schema testLibrary;
+
+create table books (
+                       id SERIAL primary key,
+                       title varchar(100) not null ,
+                       author varchar(100) not null
+);
+
+alter table books add release_year int;
--- src/test/java/DBTest.java
+++ src/test/java/DBTest.java
@@ -1,2 +1,28 @@
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.FactoryBasedNavigableListAssert.assertThat;
+
 public class DBTest {
+
+
+    @Test
+    void shouldRetrieveSavedBook() {
+        var book = sampleBook();
+        dao.save(book);
+        assertThat(dao.retrieve(book.getId()))
+                .usingRecursiveComparison()
+                .isEqualTo(book)
+                //.isNotSameAs(book)
+                ;
+    }
+
+
+    private Book sampleBook() {
+        var book = new Book();
+        book.setTitle("Java in a Nutshell");
+        book.setAuthor("Benjamin Evans and David Flanagan");
+        book.setYear(2018);
+        return book;
+    }
+
 }
