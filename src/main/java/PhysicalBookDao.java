ae2e5f0cf1ce21a4e9bbc55de5bde2a6aa1d8f6e
Marcus <60523971+Simpada@users.noreply.github.com>
2022-10-23 18:38:30 +0200

    🌶 db should list physical books by library, compiles, but gets null

---
+++ src/main/java/PhysicalBookDao.java
@@ -0,0 +1,16 @@
+import javax.sql.DataSource;
+import java.util.List;
+
+public class PhysicalBookDao {
+    
+    
+    public PhysicalBookDao(DataSource dataSource) {
+    }
+
+    public void insert(Library firstLibrary, Book firstBook) {
+    }
+
+    public List<Book> findByLibrary(Long libraryId) {
+        return null;
+    }
+}
---
+++ src/test/java/PhysicalBookDaoTest.java
@@ -0,0 +1,41 @@
+import org.junit.jupiter.api.Test;
+
+import javax.sql.DataSource;
+import java.sql.SQLException;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class PhysicalBookDaoTest {
+
+
+    private final DataSource dataSource = InMemoryDataSource.createTestDataSource();
+    private final LibraryDao libraryDao = new LibraryDao(dataSource);
+    private final BookDao bookDao = new BookDao(dataSource);
+    private final PhysicalBookDao dao = new PhysicalBookDao(dataSource);
+
+    @Test
+    void shouldListBooksByLibrary() throws SQLException {
+        var firstBook = SampleData.sampleBook();
+        var secondBook = SampleData.sampleBook();
+        bookDao.save(firstBook);
+        bookDao.save(secondBook);
+
+        var firstLibrary = SampleData.sampleLibrary();
+        var secondLibrary = SampleData.sampleLibrary();
+        libraryDao.save(firstLibrary);
+        libraryDao.save(secondLibrary);
+
+        dao.insert(firstLibrary, firstBook);
+        dao.insert(firstLibrary, secondBook);
+        dao.insert(secondLibrary, firstBook);
+
+        assertThat(dao.findByLibrary(firstLibrary.getId()))
+                .extracting(Book::getId)
+                .contains(firstBook.getId(), secondBook.getId());
+        assertThat(dao.findByLibrary(secondLibrary.getId()))
+                .extracting(Book::getId)
+                .contains(firstBook.getId())
+                .doesNotContain(secondBook.getId());
+    }
+
+}
