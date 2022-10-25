24a39b4000c911cbd2fd5d7d7fce3a73991ab26f
Marcus <60523971+Simpada@users.noreply.github.com>
2022-10-25 15:14:12 +0200

    🌲 All tests run, demo with a main also runs using flyway, hooray

--- src/test/java/no/kristiania/library/Demo.java
+++ src/test/java/no/kristiania/library/Demo.java
@@ -1,51 +1,53 @@
 package no.kristiania.library;
 
-import com.zaxxer.hikari.HikariDataSource;
-import no.kristiania.library.jdbc.JDBCBookDao;
-import no.kristiania.library.jdbc.JDBCLibraryDao;
-import no.kristiania.library.jdbc.JDBCPhysicalBookDao;
-import org.flywaydb.core.Flyway;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.Persistence;
+import no.kristiania.library.jpa.JPABookDao;
+import no.kristiania.library.jpa.JPALibraryDao;
+import org.eclipse.jetty.plus.jndi.Resource;
 
-import javax.sql.DataSource;
-import java.io.FileReader;
+
+import javax.naming.NamingException;
 import java.io.IOException;
 import java.sql.SQLException;
-import java.util.Properties;
 
 public class Demo {
 
     private final BookDao bookDao;
     private final LibraryDao libraryDao;
-    private final PhysicalBookDao physicalBookDao;
+    private final EntityManager entityManager;
+    //private final PhysicalBookDao physicalBookDao;
 
-    public Demo(DataSource dataSource){
-        this.bookDao = new JDBCBookDao(dataSource);
-        this.libraryDao = new JDBCLibraryDao(dataSource);
-        this.physicalBookDao = new JDBCPhysicalBookDao(dataSource);
+    public Demo(EntityManager entityManager) {
+        this.bookDao = new JPABookDao(entityManager);
+        this.libraryDao = new JPALibraryDao(entityManager);
+
+        this.entityManager = entityManager;
     }
 
-    public static void main(String[] args) throws SQLException, IOException {
 
-        var properties = new Properties();
-        try (var reader = new FileReader("application.properties")) {
-            properties.load(reader);
-        }
 
-        var dataSource = new HikariDataSource();
-        dataSource.setJdbcUrl(properties.getProperty("jdbc.url"));
-        dataSource.setUsername(properties.getProperty("jdbc.username"));
-        dataSource.setPassword(properties.getProperty("jdbc.password"));
-        Flyway.configure().dataSource(dataSource).load().migrate();
-        new Demo(dataSource).run();
+    public static void main(String[] args) throws SQLException, IOException, NamingException {
+
+        new Resource("jdbc/dataSource", Database.getDataSource());
+        var entityManager = Persistence
+                .createEntityManagerFactory("library")
+                .createEntityManager();
+        new Demo(entityManager).run();
     }
 
     private void run() throws SQLException {
+
+        entityManager.getTransaction().begin();
+
         var book = SampleData.sampleBook();
         bookDao.save(book);
         var library = SampleData.sampleLibrary();
         libraryDao.save(library);
 
-        physicalBookDao.insert(library, book);
+        entityManager.flush();
+        entityManager.getTransaction().commit();
+        //physicalBookDao.insert(library, book);
     }
 
 }
