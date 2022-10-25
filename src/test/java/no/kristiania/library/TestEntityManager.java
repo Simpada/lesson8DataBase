d99883be5fefc09d7937e06e834e75d40cf3ddb4
Marcus <60523971+Simpada@users.noreply.github.com>
2022-10-25 14:36:08 +0200

    Changed to use datasource and flyway instead of hardcoded information

--- src/test/java/no/kristiania/library/TestEntityManager.java
+++ src/test/java/no/kristiania/library/TestEntityManager.java
@@ -2,8 +2,10 @@ package no.kristiania.library;
 
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.Persistence;
+import org.eclipse.jetty.plus.jndi.Resource;
 import org.junit.jupiter.api.extension.*;
 
+import javax.naming.NamingException;
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
@@ -13,7 +15,7 @@ import java.lang.annotation.Target;
 @Retention(RetentionPolicy.RUNTIME)
 @ExtendWith(TestEntityManager.Implementation.class)
 public @interface TestEntityManager {
-    public class Implementation implements ParameterResolver, BeforeEachCallback, AfterEachCallback {
+    class Implementation implements ParameterResolver, BeforeEachCallback, AfterEachCallback {
 
         @Override
         public boolean supportsParameter(ParameterContext parameterContext, ExtensionContext extensionContext) throws ParameterResolutionException {
@@ -22,6 +24,13 @@ public @interface TestEntityManager {
 
         @Override
         public Object resolveParameter(ParameterContext parameterContext, ExtensionContext context) throws ParameterResolutionException {
+
+            try {
+                new Resource("jdbc/dataSource", InMemoryDataSource.createTestDataSource());
+            } catch (NamingException e) {
+                throw new ParameterResolutionException(e.toString());
+            }
+
             var entityManager = Persistence.createEntityManagerFactory("library").createEntityManager();
             context.getStore(ExtensionContext.Namespace.GLOBAL).put("entityManager", entityManager);
             return entityManager;
